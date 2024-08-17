cc860c0f89ef133c5172bc65190c936b2dee76cf
JSBelka <barbi-bar@mail.ru>
2024-08-17 21:28:19 +0300

    changes in index.html

--- shelter/index.js
+++ shelter/index.js
@@ -1,14 +1,42 @@
-const card = document.getElementById('Jennifer');
-const popUp = document.querySelector('.pop-up');
-const button = document.querySelector('.pop-up-button');
+let card = document.getElementById('Jennifer');
+let popUp = document.querySelector('.pop-up');
+let button = document.querySelector('.pop-up-button');
 
 card.addEventListener("click", function() {
     popUp.classList.toggle("hidden");
     document.body.style.overflow = "hidden";
-    })
+})
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function() {
     popUp.classList.toggle("hidden");
     document.body.style.overflow = "";
+})
+
+// Burger-menu
+let burger = document.querySelector('.header-burger');
+let burgerMenu = document.querySelector('.burger-menu');
+let nav = document.querySelector('.nav');
+let navList = document.querySelector('.nav-list');
+let navItem = document.querySelector('.nav-item');
+let body = document.querySelector('body');
+let background = document.getElementById('main-page')
+
+burger.addEventListener("click", function (event) {
+    nav.classList.toggle("active");
+    burger.classList.toggle("active-burger");
+    body.classList.toggle("stop");
+    background.classList.toggle("dark");
+})
+
+let navItems = [...navItem].concat(nav, navList, burger, burgerMenu);
+
+document.addEventListener("click", function(event) {
+    if(!navItems.includes(event.target)){
+        nav.classList.remove("active");
+        burger.classList.remove("active-burger");
+        body.classList.remove("stop");
+        background.classList.remove("dark");
     }
-)
+})
+
+
