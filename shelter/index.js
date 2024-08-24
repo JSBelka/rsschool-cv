a7671d01b411ddeb61ed60ae17b8dcb0a37282c7
JSBelka <barbi-bar@mail.ru>
2024-08-24 16:04:09 +0300

    changes in index.html

--- shelter/index.js
+++ shelter/index.js
@@ -5,13 +5,24 @@ let cardKatrine = document.getElementById('Katrine');
 let popUpKatrine = document.getElementById('pop-up-Katrine');
 let cardWoody = document.getElementById('Woody');
 let popUpWoody = document.getElementById('pop-up-Woody');
+let cardSophia = document.getElementById('Sophia');
+let popUpSophia = document.getElementById('pop-up-Sophia');
+let cardTimmy = document.getElementById('Timmy');
+let popUpTimmy = document.getElementById('pop-up-Timmy');
+let cardCharly = document.getElementById('Charly');
+let popUpCharly = document.getElementById('pop-up-Charly');
+let cardScarlett = document.getElementById('Scarlett');
+let popUpScarlett = document.getElementById('pop-up-Scarlett');
+let cardFreddie = document.getElementById('Freddie');
+let popUpFreddie = document.getElementById('pop-up-Freddie');
 let button = document.querySelector('.pop-up-button');
 
+//Katrine
 cardKatrine.addEventListener("click", function() {
     popUpKatrine.classList.toggle("hidden");
     body.classList.toggle("stop");
 })
-
+/*настроить закрытие по кнопке и экрану!!!
 document.addEventListener("click", function(event) {
     if(!popUpKatrine.includes(event.target)) {
         popUpKatrine.classList.toggle("hidden");
@@ -23,44 +34,52 @@ button.addEventListener("click", function(event) {
         popUpKatrine.classList.toggle("hidden");
         body.classList.remove("stop");
     }
-})
+})*/
 
-
+/*document.addEventListener("click", function(event) {
+    if (!popUpKatrine.includes(event.target) || !popUpJennifer.includes(event.target) || !popUpWoody.includes(event.target)) {
+        popUpKatrine.classList.toggle("hidden");
+        popUpWoody.classList.toggle("hidden");
+        popUpJennifer.classList.toggle("hidden");
+        body.classList.remove("stop");
+    }
+}*/
+//Jennifer
 cardJennifer.addEventListener("click", function() {
     popUpJennifer.classList.toggle("hidden");
     body.classList.toggle("stop");
 })
-
-document.addEventListener("click", function(event) {
-        if(!popUpJennifer.includes(event.target)) {
-            popUpJennifer.classList.toggle("hidden");
-            body.classList.remove("stop");
-        }
-})
-button.addEventListener("click", function(event) {
-    if(!popUpJennifer.includes(event.target)) {
-        popUpJennifer.classList.toggle("hidden");
-        body.classList.remove("stop");
-    }
-})
-
+//Woody
 cardWoody.addEventListener("click", function() {
     popUpWoody.classList.toggle("hidden");
     body.classList.toggle("stop");
 })
+//Sophia
+cardSophia.addEventListener("click", function() {
+    popUpSophia.classList.toggle("hidden");
+    body.classList.toggle("stop");
+})
+//Timmy
+cardTimmy.addEventListener("click", function() {
+    popUpTimmy.classList.toggle("hidden");
+    body.classList.toggle("stop");
+})
+//Charly
+cardCharly.addEventListener("click", function() {
+    popUpCharly.classList.toggle("hidden");
+    body.classList.toggle("stop");
+})
+//Scarlett
+cardScarlett.addEventListener("click", function() {
+    popUpScarlett.classList.toggle("hidden");
+    body.classList.toggle("stop");
+})
+//Freddie
+cardFreddie.addEventListener("click", function() {
+    popUpFreddie.classList.toggle("hidden");
+    body.classList.toggle("stop");
+})
 
-document.addEventListener("click", function(event) {
-    if(!popUpWoody.includes(event.target)) {
-        popUpWoody.classList.toggle("hidden");
-        body.classList.remove("stop");
-    }
-})
-button.addEventListener("click", function(event) {
-    if(!popUpWoody.includes(event.target)) {
-        popUpWoody.classList.toggle("hidden");
-        body.classList.remove("stop");
-    }
-})
 
 // Burger-menu
 let burger = document.querySelector('.header-burger');
@@ -89,3 +108,26 @@ document.addEventListener("click", function(event) {
     }
 })
 
+// Slider
+
+let arrowRight = document.getElementById('button-right');
+let arrowLeft = document.getElementById('button-left');
+let slider = document.querySelector(".cards-pets")
+const cardWidth = document.querySelector(".card").offsetWidth + 20;
+const totalCards = document.querySelectorAll(".card").length;
+let currentPosition = 0;
+
+arrowRight.addEventListener("click", () => {
+    slider.classList.toggle("active-slider");
+});
+/*arrowLeft.addEventListener("click", () => {
+    currentPosition -= cardWidth;
+    if (currentPosition <= -cardWidth * (totalCards - 3)) {
+        currentPosition = -cardWidth * (totalCards - 3);
+    }
+    sliderTrack.style.transform = translateX(${currentPosition}px);
+});*/
+
+/*arrowRight.addEventListener("click", function {
+    slider.classList.toggle("active-slider");
+})*/
